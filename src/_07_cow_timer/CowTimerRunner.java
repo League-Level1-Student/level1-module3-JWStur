2f5f3db37557a132f7c24a814f8f7a722b424cd3
student <student@ip-172-31-49-20.us-west-2.compute.internal>
2021-08-07 14:01:03 -0700

    DONE!!!

--- src/_07_cow_timer/CowTimerRunner.java
+++ src/_07_cow_timer/CowTimerRunner.java
@@ -2,6 +2,9 @@ package _07_cow_timer;
 
 public class CowTimerRunner {
 	public static void main(String[] args) throws InterruptedException {
+		CowTimer timer = new CowTimer();
+		timer.setTime(1);
+		timer.start();
 		/* Make a CowTimer, set its time and start it.
 		 * Use a short delay (seconds) when testing, then try with longer delays */
 
